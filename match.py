3b11ca430996f17ee4ce55f4cefad0dce0702b88
Phan Huy Cuong <63645445+azuredragon3000@users.noreply.github.com>
2024-12-04 17:28:53 +0700

    Update match.py
    new version

--- match.py
+++ match.py
@@ -1,5 +1,4 @@
 import pyautogui
-import numpy as np
 from env2 import Match3MouseEnv
 from env2 import find_potential_matches
 from env2 import startGame
@@ -8,15 +7,20 @@ from time import sleep
 EPISODES = 2000
     
 if __name__ == "__main__":
-    #startGame()
-    env = Match3MouseEnv()
-    check = False
+    env = Match3MouseEnv() # reset enviroment
+    #check = False # 
+    # Đường dẫn tới hình ảnh
+    image_path = "cropped_image.png"
+    pattern_folder = "C:\MyStore\workspace\pt" 
+    output_folder = "C:\MyStore\workspace\image"
+    screenshot = "screenshot.png"
+    cropped_image = "cropped_image.png"
+    
     for episode in range(EPISODES):
         print(f"Episode {episode + 1}/{EPISODES}")
-        #tam thoi nen xoa sau nay
-        current_state = env.reset()
-        print(env.check)
-        if env.check:
-            matches = find_potential_matches(current_state, threshold=2)
-            env.step(matches)
-            print(" ok i done step")
+        current_state = env.reset(image_path,pattern_folder,output_folder,screenshot,cropped_image) # reset env and return matrix
+        matches = find_potential_matches(current_state, threshold=2)
+        env.step(matches)
+        #print(env.check)
+        #if env.check:   
+        #print(" ok i done step")
